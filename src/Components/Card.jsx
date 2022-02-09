6478ef6497368de127c2a349c4a4da2d669a9ec3
Irullatif <irulsoekamti94@gmail.com>
2022-02-10 01:25:18 +0700

    Add Card Component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="border-2 rounded-xl">
+      <h1>Judul</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus illo
+        maiores quam quos sint itaque? Facilis excepturi vel nesciunt
+        voluptatum?
+      </p>
+    </div>
+  );
+}
