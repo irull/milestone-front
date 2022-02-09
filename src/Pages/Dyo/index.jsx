caa38e2a28e9f2d86644ce40223c5708c79da4c9
Irullatif <irulsoekamti94@gmail.com>
2022-02-10 01:27:10 +0700

    add met title (React Helmet)

--- src/Pages/Dyo/index.jsx
+++ src/Pages/Dyo/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function Dyo() {
   return (
-    <div>
-      <h1>Halaman Dyo</h1>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>DYO | Draw Your Own</title>
+      </Helmet>
+      <div>
+        <h1>Halaman Dyo</h1>
+      </div>
+    </>
   );
 }
 
