9b7f4db8ef6d4a11fc53083f57311510f7b29848
Irullatif <irulsoekamti94@gmail.com>
2022-02-10 01:26:42 +0700

    included Button, Card Component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import Button from "../../Components/Button";
+import Card from "../../Components/Card";
 
 function Home() {
   return (
     <>
-      <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home</title>
+      </Helmet>
+      <div className="text-center space-x-2">
         <h1>Halaman Home</h1>
+        <button className="bg-blue-500 px-6 py-2.5 rounded-full font-semibold text-white hover:bg-blue-600 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-200">
+          Button inline
+        </button>
+        <Button>Hallo button primary</Button>
+        <Button>Hallo button secondary</Button>
+        <Button className="bg-cyan-500 focus:ring-cyan-200 hover:bg-cyan-700">
+          Hallo
+        </Button>
+        <div className="flex mx-5">
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </>
   );
