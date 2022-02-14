cd90ea6aeac6d290e126b09676906e998cae3de1
irullatif <irulsoekamti94@gmail.com>
2022-02-15 01:52:14 +0700

    Update Dropdown Mobile version

--- src/Components/MobileNav.jsx
+++ src/Components/MobileNav.jsx
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { RiMenu4Fill } from "react-icons/ri";
+import { FaRegMoon, FaShoppingBag, FaAngleDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Line from "./Line";
 
 export default function MobileNav() {
   return (
@@ -12,9 +14,13 @@ export default function MobileNav() {
       <Link to="/" className="font-semibold text-md text-white">
         milestone
       </Link>
-      <Menu.Button>
-        <RiMenu4Fill size={30} className="text-white focus:outline-none" />
-      </Menu.Button>
+      <div className="flex items-center">
+        <FaRegMoon size={20} className="text-white mr-3" />
+        <FaShoppingBag size={23} className="text-white mr-3" />
+        <Menu.Button>
+          <RiMenu4Fill size={30} className="text-white focus:outline-none" />
+        </Menu.Button>
+      </div>
       <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
@@ -26,71 +32,63 @@ export default function MobileNav() {
       >
         <Menu.Items
           as="div"
-          className="absolute bg-gray-100 right-0 mr-0 top-0 rounded-lg mt-12 py-1 w-full overflow-hidden text-sm font-medium"
+          className="absolute bg-gray-900 top-0 rounded-b-2xl left-0 right-0 mr-auto ml-auto  mt-12 py-1 w-full overflow-hidden text-sm font-medium"
         >
           <Menu.Item>
             <Link
-              to="/"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
-            >
-              Home
-            </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link
               to="/dyo"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
+              className="px-4 py-2 hover:bg-slate-200 text-gray-200 hover:text-black flex"
             >
               DYO
             </Link>
           </Menu.Item>
-          <div className="w-full h-px bg-gray-300 my-1" />
+          <Menu.Item>
+            <Link
+              to="/shop"
+              className="px-4 py-2 hover:bg-gray-100 text-gray-200 hover:text-black flex items-center"
+            >
+              Shop <FaAngleDown className="ml-1" />
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link
+              to="/orderby"
+              className="px-4 py-2 hover:bg-gray-100 text-gray-200 hover:text-black flex items-center"
+            >
+              Order by <FaAngleDown className="ml-1" />
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link
+              to="/about"
+              className="px-4 py-2 hover:bg-gray-100 text-gray-200 hover:text-black flex"
+            >
+              About Us
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link
+              to="/contact"
+              className="px-4 py-2 hover:bg-gray-100 text-gray-200 hover:text-black flex"
+            >
+              Contact Us
+            </Link>
+          </Menu.Item>
           <Menu.Item>
             <Link
               to="/blog"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
+              className="px-4 py-2 hover:bg-gray-100 text-gray-200 hover:text-black flex"
             >
               Blog
             </Link>
           </Menu.Item>
+          <Line />
           <Menu.Item>
             <Link
-              to="/contact"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
+              to="/login"
+              className="px-4 py-2 hover:bg-gray-100 text-gray-200 hover:text-black flex"
             >
-              Contact Us
-            </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link
-              to="/contact"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
-            >
-              Contact Us
-            </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link
-              to="/contact"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
-            >
-              Contact Us
-            </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link
-              to="/contact"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
-            >
-              Contact Us
-            </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link
-              to="/contact"
-              className="px-4 py-2 hover:bg-gray-100 text-gray-700 hover:text-black block"
-            >
-              Contact Us
+              Login
             </Link>
           </Menu.Item>
         </Menu.Items>
