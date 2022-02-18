028b70f6301ac21d44e35f7783811cab31574b26
irullatif <irulsoekamti94@gmail.com>
2022-02-18 14:28:15 +0700

    Update to carousel component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,17 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-// import { Link } from "react-router-dom";
-// import Button from "../../Components/Button";
-// import Card from "../../Components/Card";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination } from "swiper";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-
-// import "./styles.css";
+import Carousel from "../../Components/Carousel";
 
 function Home() {
   return (
@@ -20,85 +9,7 @@ function Home() {
         <meta charSet="utf-8" />
         <title>Milestone</title>
       </Helmet>
-
-      <div className="">
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={"auto"}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          pagination={true}
-          modules={[EffectCoverflow, Pagination]}
-          className=" py-10"
-        >
-          <SwiperSlide
-            as="div"
-            className="bg-center bg-cover items-center block"
-          >
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-          <SwiperSlide as="div" className="bg-center bg-cover w-80 h-80">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-          <SwiperSlide as="div" className="bg-center bg-cover w-80 h-80">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-          <SwiperSlide as="div" className="bg-center bg-cover w-80 h-80">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-          <SwiperSlide as="div" className="bg-center bg-cover w-80 h-80">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-          <SwiperSlide as="div" className="bg-center bg-cover w-80 h-80">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-          <SwiperSlide as="div" className="bg-center bg-cover w-80 h-80">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-          <SwiperSlide as="div" className="bg-center bg-cover w-80 h-80">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="slide"
-              className="block w-96"
-            />
-          </SwiperSlide>
-        </Swiper>
-      </div>
+      <Carousel />
     </>
   );
 }
